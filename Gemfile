88b4dba557295becac3f2256c8ca0392f8520518
saul appelbaum <saul@veramaurinapress.org>
2015-06-07 17:24:06 +0700

    with picture uploader

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.2.1'
 
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
+gem 'carrierwave'
 
 group :development do
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :grids
-  root 'grids#index'
+  root :to => redirect('/grids')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
