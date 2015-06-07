2587661c2a96175aed3cf900b7909dafdd0bbc88
saul appelbaum <saul@veramaurinapress.org>
2015-06-07 17:34:39 +0700

    fixed hidden field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607092320) do
+ActiveRecord::Schema.define(version: 20150607102712) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "grid_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "grids", force: :cascade do |t|
     t.text     "title"
